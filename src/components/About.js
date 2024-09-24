774f244f2cdfb5b8936b874bf45bae395209ef45
h-kawatoko <hayate_kawatoko@respawn.jp>
2024-09-24 22:48:00 +0900

    [about]スタイル修正

--- src/components/About.js
+++ src/components/About.js
@@ -20,7 +20,7 @@ function About() {
                         </motion.div>
 
                         <motion.div
-                            className=""
+                            id="prof-right-block"
                             initial={{ opacity: 0, x: +100 }}
                             animate={{ opacity: 1, x: 0 }}
                             transition={{ duration: 2 }}
