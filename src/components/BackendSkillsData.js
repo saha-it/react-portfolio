ee5b1607addfaecd6ef6e077a05f9e54b2d2aad5
h-kawatoko <hayate_kawatoko@respawn.jp>
2024-09-26 17:08:38 +0900

    スライダーをReact使用に書き換え

---
+++ src/components/BackendSkillsData.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+/*images*/
+import img_php from "../images/php.png";
+import img_laravel from "../images/Laravel.png";
+import img_livewire from "../images/Livewire.png";
+
+export const BackendSkillsData = [
+    {
+        image: img_php,
+        skill_name: "PHP",
+        star: "4",
+        skill_description:
+            "約1年ほど実務経験があります。MVCモデルやルーティングの理解はできています。",
+    },
+    {
+        image: img_laravel,
+        skill_name: "Laravel",
+        star: "4",
+        skill_description:
+            "こちらも1年ほどの実務経験があります。DBの操作に加え、エンドポイントの作成、APIの呼び出しなども可能です。",
+    },
+    {
+        image: img_livewire,
+        skill_name: "livewire",
+        star: "4",
+        skill_description:
+            "動的なページをPHPのみで可能にするライブラリです。1年ほど実務経験があります。",
+    },
+];
