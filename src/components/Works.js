e7fb8e3d735a7979121cdf31583b0017d8458582
h-kawatoko <hayate_kawatoko@respawn.jp>
2024-09-27 03:45:02 +0900

    [works]Reactに書き換え

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,42 +1,37 @@
 import React from "react";
 
+import { WorksData } from "./WorksData";
+
 function Works() {
     return (
         <div>
             <section>
-                <div class="section-wapper" id="works">
-                    <h2 class="heading">works</h2>
-                    {/* <div class="works-block-wrapper">
-                        <div class="works-block">
-                            <img src="brand-images/clothing.jpg"></img>
-                            <a href="./brand.html">
-                                中古ハイブランド買取サイト(架空サイト)
-                            </a>
-                            <p>プロジェクトで実装したページの再現です。</p>
-                        </div>
-                        <div class="works-block">
-                            <img src="LP/cafe/images/TOP.jpg"></img>
-                            <a href="LP/cafe/cafe.html">cafe</a>
-                        </div>
-                        <div class="works-block">
-                            <div class="mock-img-block">
-                                <img
-                                    src="images/mock-company-pc.png"
-                                    class="mock-pc"
-                                ></img>
-                                <img
-                                    src="images/mock-company-sp.png"
-                                    class="mock-sp"
-                                ></img>
-                            </div>
-                            <div class="works-text-block">
-                                <a href="LP/company/company.html">
-                                    架空企業サイト(架空サイト)
-                                </a>
-                                <p>デザインの勉強として作成しました。</p>
-                            </div>
-                        </div>
-                    </div> */}
+                <div className="section-wapper" id="works">
+                    <h2 className="heading">works</h2>
+                    <div className="works-block-wrapper">
+                        {WorksData.map((value, key) => {
+                            return (
+                                <div className="works-block" key={key}>
+                                    <div className="mock-img-block">
+                                        <img
+                                            src={value.img_pc}
+                                            className="mock-pc"
+                                        ></img>
+                                        <img
+                                            src={value.img_sp}
+                                            className="mock-sp"
+                                        ></img>
+                                    </div>
+                                    <div className="works-text-block">
+                                        <a href="LP/company/company.html">
+                                            {value.link_title}
+                                        </a>
+                                        <p>{value.message}</p>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             </section>
         </div>
