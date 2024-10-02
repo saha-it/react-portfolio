680bc9a9640a355abe4a412fc20bb411d894983b
h-kawatoko <hayate_kawatoko@respawn.jp>
2024-10-02 11:18:38 +0900

    リンク部分修正

--- src/components/WorksData.js
+++ src/components/WorksData.js
@@ -3,11 +3,13 @@ import React from "react";
 import img_company_pc from "../images/mock-company-pc.png";
 import img_company_sp from "../images/mock-company-sp.png";
 
+const company = "https://saha-it.github.io/portfolio/LP/company/company.html";
+
 export const WorksData = [
     {
         img_pc: img_company_pc,
         img_sp: img_company_sp,
-        link: "test",
+        link: company,
         link_title: "架空企業のWebサイト（自主制作）",
         message: "HTML・CSS・Javascriptの練習として制作しました。",
     },
