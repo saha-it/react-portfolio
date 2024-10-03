7d0c367f2105fe28d5b4f2cd090f8377490eec51
h-kawatoko <hayate_kawatoko@respawn.jp>
2024-10-03 15:17:06 +0900

    SKILLSのスライダー調整

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,30 @@
 import React from "react";
 
+import SkillsSlide from "./SkillsSlide";
+
 /*slide*/
 import "swiper/css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Pagination, Navigation } from "swiper/modules";
+import { Pagination, Navigation, EffectCards } from "swiper/modules";
+import "swiper/css/effect-cards";
 
 /*slide data*/
 import { FrontendSkillsData } from "./FrontendSkillsData";
 import { BackendSkillsData } from "./BackendSkillsData";
 import { OthersSkillsData } from "./OthersSkillsData";
+import { transform } from "framer-motion";
+import { style } from "framer-motion/client";
+
+const styles = {
+    /* swiperSlide: {
+        transform: "unset",
+    }, */
+    swiperButtonPrev: {
+        left: "0",
+    },
+};
 
 function Skills() {
     return (
@@ -19,91 +33,35 @@ function Skills() {
                 <div className="section-wapper" id="skills">
                     <h2 className="heading">skills</h2>
                     <div className="skills-block-wrapper">
-                        <p className="skill-type">FRONTEND</p>
-                        <ul className="skills-list">
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>Tailwind</li>
-                            <li>daisyUI</li>
-                            <li>JavaScript</li>
-                            <li>jQuery</li>
-                            <li>TypeScript</li>
-                            <li>Vue.js</li>
-                        </ul>
-                        <Swiper
-                            spaceBetween={50}
-                            slidesPerView={1}
-                            loop={true}
-                            pagination={{
-                                clickable: true,
-                            }}
-                            navigation={true}
-                            modules={[Pagination, Navigation]}
-                            className="skills-block"
-                        >
-                            {FrontendSkillsData.map((value, key) => (
-                                <SwiperSlide
-                                    key={key}
-                                    className="slider skills-items js-slick"
-                                >
-                                    <li>
-                                        <img src={value.image}></img>
-                                        <p className="skill-title">
-                                            {value.skill_name}
-                                        </p>
-                                        <span
-                                            className="star5_rating"
-                                            data-rate={value.star}
-                                        ></span>
-                                        <div className="skill-text-block">
-                                            <p>{value.skill_description}</p>
-                                        </div>
-                                    </li>
-                                </SwiperSlide>
-                            ))}
-                        </Swiper>
-                        <p className="skill-type">BACKEND</p>
                         <div className="skills-block">
+                            <p className="skill-type">FRONTEND</p>
+                            <ul className="skills-list">
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>Tailwind</li>
+                                <li>daisyUI</li>
+                                <li>JavaScript</li>
+                                <li>jQuery</li>
+                                <li>TypeScript</li>
+                                <li>Vue.js</li>
+                            </ul>
+                            <SkillsSlide
+                                SkillsDataObject={FrontendSkillsData}
+                            />
+                        </div>
+
+                        <div className="skills-block">
+                            <p className="skill-type">BACKEND</p>
                             <ul className="skills-list">
                                 <li>PHP</li>
                                 <li>Laravel</li>
                                 <li>livewire</li>
                             </ul>
-                            <Swiper
-                                spaceBetween={50}
-                                slidesPerView={1}
-                                loop={true}
-                                pagination={{
-                                    clickable: true,
-                                }}
-                                navigation={true}
-                                modules={[Pagination, Navigation]}
-                                className="skills-block"
-                            >
-                                {BackendSkillsData.map((value, key) => (
-                                    <SwiperSlide
-                                        key={key}
-                                        className="slider skills-items js-slick"
-                                    >
-                                        <li>
-                                            <img src={value.image}></img>
-                                            <p className="skill-title">
-                                                {value.skill_name}
-                                            </p>
-                                            <span
-                                                className="star5_rating"
-                                                data-rate={value.star}
-                                            ></span>
-                                            <div className="skill-text-block">
-                                                <p>{value.skill_description}</p>
-                                            </div>
-                                        </li>
-                                    </SwiperSlide>
-                                ))}
-                            </Swiper>
+                            <SkillsSlide SkillsDataObject={BackendSkillsData} />
                         </div>
-                        <p className="skill-type">OTHERS</p>
+
                         <div className="skills-block">
+                            <p className="skill-type">OTHERS</p>
                             <ul className="skills-list">
                                 <li>MySQL</li>
                                 <li>PostgreSQL</li>
@@ -111,38 +69,7 @@ function Skills() {
                                 <li>Docker</li>
                                 <li>API</li>
                             </ul>
-                            <Swiper
-                                spaceBetween={50}
-                                slidesPerView={1}
-                                loop={true}
-                                pagination={{
-                                    clickable: true,
-                                }}
-                                navigation={true}
-                                modules={[Pagination, Navigation]}
-                                className="skills-block"
-                            >
-                                {OthersSkillsData.map((value, key) => (
-                                    <SwiperSlide
-                                        key={key}
-                                        className="slider skills-items js-slick"
-                                    >
-                                        <li>
-                                            <img src={value.image}></img>
-                                            <p className="skill-title">
-                                                {value.skill_name}
-                                            </p>
-                                            <span
-                                                className="star5_rating"
-                                                data-rate={value.star}
-                                            ></span>
-                                            <div className="skill-text-block">
-                                                <p>{value.skill_description}</p>
-                                            </div>
-                                        </li>
-                                    </SwiperSlide>
-                                ))}
-                            </Swiper>
+                            <SkillsSlide SkillsDataObject={OthersSkillsData} />
                         </div>
                     </div>
                 </div>
